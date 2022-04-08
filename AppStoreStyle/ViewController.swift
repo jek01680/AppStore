a94a4b8faf455408b6afa0086521560a1b3ff295
jek01680 <jek01680@icloud.com>
2022-04-08 14:17:49 +0900

    Initial Commit

---
+++ AppStoreStyle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreStyle
+//
+//  Created by 정은경 on 2022/04/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
